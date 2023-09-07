5cecb872db8d680592217f36d7f7312883ce253e
SholaInspire <shorlar96@gmail.com>
2023-09-07 14:28:08 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - c programming is awesome
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
