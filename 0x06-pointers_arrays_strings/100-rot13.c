40ff16fc1710511195f435f322c85ee8bba13e44
SholaInspire <shorlar96@gmail.com>
2023-09-21 21:21:09 +0100

    Task 100 - rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - function that encodes a string.
+ * @s: pointer to the string.
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int a;
+	int b;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; b < 52; b++)
+		{
+			if (s[a] == data1[b])
+			{
+				s[a] = datarot[b];
+				break;
+			}
+		}
+	}
+	return (s);
+}
