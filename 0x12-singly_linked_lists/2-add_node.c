725a5201b005a0ee5521e0631de9dfd5b2b1d2d9
SholaInspire <shorlar@gmail.com>
2023-10-19 14:02:18 -0700

    Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node
+ * @head: head
+ * @str: string
+ *
+ * Return: the address of the new element or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len] != '\0')
+		len++;
+	new_node = malloc(sizeof(list_t));
+
+			if (new_node == NULL)
+			return (NULL);
+
+			new_node->len = len;
+			new_node->str = strdup(str);
+
+			if (new_node->str == NULL)
+			{
+				free(new_node);
+				return (NULL);
+			}
+
+			new_node->next = *head;
+			*head = new_node;
+
+			return (new_node);
+}
